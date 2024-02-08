199ad905a673b035c668af04aef615d0e6396f96
vendarasan <130369906+vendarasan@users.noreply.github.com>
2024-02-08 15:34:27 +0530

    work files

---
+++ work/workjs.js
@@ -0,0 +1,16 @@
+var addbutton = document.querySelector(".add-button")
+var fulldiv = document.querySelector(".full-div")
+var inputbox = document.querySelector(".inputbox")
+addbutton.addEventListener("click",function(){
+    fulldiv.style.display="block"
+    inputbox.style.display="block"
+
+})
+var importbutton = document.querySelector(".import-button")
+var importdiv = document.querySelector(".import-div")
+var importbox = document.querySelector(".importbox")
+importbutton.addEventListener("click",function(){
+    importdiv.style.display="block"
+    importbox.style.display="block"
+
+})
